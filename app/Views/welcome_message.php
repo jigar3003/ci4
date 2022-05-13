c0f0dde97295f31b25183ef32ba9482b37a82106
Vaibhav Patel <vaibhav.mobilefirst@gmail.com>
2022-05-13 08:50:24 +0530

    Complete project

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -8,7 +8,7 @@
 	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
 
 	<!-- STYLES -->
-<?php
+
 	<style {csp-style-nonce}>
 		* {
 			transition: background-color 300ms ease, color 300ms ease;
